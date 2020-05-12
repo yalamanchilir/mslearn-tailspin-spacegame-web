5c8d6bcfc2795149901cb05631869528a14fc958
Ram Yalamanchili <sreya.yalamanchili@hotmail.com>
2020-05-12 11:59:52 -0500

    Return correct number of items

--- Tailspin.SpaceGame.Web/LocalDocumentDBRepository.cs
+++ Tailspin.SpaceGame.Web/LocalDocumentDBRepository.cs
@@ -54,14 +54,13 @@ namespace TailSpin.SpaceGame.Web
         public Task<IEnumerable<T>> GetItemsAsync(
             Expression<Func<T, bool>> queryPredicate,
             Expression<Func<T, int>> orderDescendingPredicate,
-            int page = 1, int pageSize = 10
-        )
+            int page = 1, int pageSize = 10)
         {
             var result = _items.AsQueryable()
                 .Where(queryPredicate) // filter
                 .OrderByDescending(orderDescendingPredicate) // sort
                 .Skip(page * pageSize) // find page
-                .Take(pageSize - 1) // take items
+                .Take(pageSize) // take items
                 .AsEnumerable(); // make enumeratable
 
             return Task<IEnumerable<T>>.FromResult(result);
